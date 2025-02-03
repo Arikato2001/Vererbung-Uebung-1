51c9878d2e10e3bd1e29c9626614ee507209f50d
MSI\Arika <marigorelishvili9@gmail.com>
2025-02-03 12:16:32 +0100

    Commit 1

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        Supervisor sup = new Supervisor("John", "Doe", 40, "S123", "S123", 5000.0);
+        Employee emp = new Employee("Jane", "Smith", 30, "E456", "S123", 3000.0);
+
+        emp.printAll();
+
+        // Supervisor changes the employee's salary
+        sup.changeSalary(emp, 3500.0);
+
+        System.out.println("\nFinal Employee Details:");
+        emp.printAll();
+    }
+}
